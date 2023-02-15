bad21f9ac1f8c9f8cbcf4bdda707088fb48179c3
yuki546 <diesunabe@gmail.com>
2023-02-16 00:59:49 +0900

    Add files via upload

---
+++ test-site/scripts/main.js
@@ -0,0 +1,37 @@
+// Image switcher code
+const myImage = document.querySelector('img');
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute('src');
+  if (mySrc === 'images/firefox-icon.png') {
+    myImage.setAttribute('src', 'images/firefox2.png');
+  } else {
+    myImage.setAttribute('src','images/firefox-icon.png');
+  }
+}
+
+// Personalized welcome message code
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  const myName = prompt('あなたの名前を入力してください。');
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = `Mozilla はかっこいいよ、${myName}`;
+  }
+}
+
+if (!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem('name');
+  myHeading.textContent = `Mozilla はかっこいいよ、${storedName}`;
+}
+
+myButton.onclick = () => {
+  setUserName();
+}
